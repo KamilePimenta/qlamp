b254c239c6d93ac303e861cb40902cc495643d7e
Daniel Bispo <daniel@tmw.com.br>
2016-08-29 15:17:35 -0300

    Modificados endereços de email para envio

--- enviadados.php
+++ enviadados.php
@@ -2,56 +2,57 @@
 require_once 'Saida.php';
 
 // Verifica se foi enviada uma foto (se não é acesso direto)
-if (!isset($_FILES['foto'])) {
+if (! isset($_FILES[ 'foto' ])) {
     Saida::json('Acesso Negado', true);
 }
 
 // Pega os dados enviados
-$foto = $_FILES['foto'];
-$nome = filter_input(INPUT_POST, 'nome');
-$email = filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL);
-$msg = filter_input(INPUT_POST, 'msg');
+$foto = $_FILES[ 'foto' ];
+$nomeCliente = filter_input(INPUT_POST, 'nome');
+$emailCliente = filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL);
+$msgCliente = filter_input(INPUT_POST, 'msg');
 
 // Verificando email
-if (empty($email)) {
+if (empty($emailCliente)) {
     Saida::json('Por favor, nos informe um email válido', true);
 }
 
 // Verificando imagem
-if (!isset($foto['tmp_name']) || empty($foto['tmp_name']) || !preg_match('/image/i', $foto['type'])) {
+if (! isset($foto[ 'tmp_name' ]) || empty($foto[ 'tmp_name' ]) || ! preg_match('/image/i', $foto[ 'type' ])) {
     Saida::json('Envie-nos uma foto do ambiente para que possamos indicar a melhor lâmpada para você', true);
 }
 
-// Pegando o email da loja
-require_once '../config/conecta.class.php';
-$pdo = new Conecta();
-$sacLoja = 'daniel@tmw.com.br'; #$pdo->execute('SELECT CON_EMAIL_LOJA FROM config', true)->CON_EMAIL_LOJA;
+// Email de origem (tem que ser um @aiha.com.br)
+$emailOrigem = 'sac@aiha.com.br';
+
+// Email para onde será enviado
+$emailDestino = 'kaled@aiha.com.br';
 
 // Mensagem email
 $msgMail = preg_replace('/[\n\r\s\t]+/', ' ',
     "<html><body>
         <p>Olá, gostaria de saber qual a melhor lâmpada para meu ambiente.</p>
         <p>
-            <strong>Nome: </strong> $nome<br>
-            <strong>Email:</strong> $email<br>
-            <strong>Mensagem:</strong> $msg
+            <strong>Nome: </strong> $nomeCliente<br>
+            <strong>Email:</strong> $emailCliente<br>
+            <strong>Mensagem:</strong> $msgCliente
         </p>
-        <p><small>Imagem em anexo.</small></p>
+        <p>Email enviado de <a href='https://www.aiha.com.br/qlamp/' target='_blank'>Qual é a lâmpada</a> <small>(Imagem em anexo)</small>)</p>
     </body></html>"
 );
 
 // Setando o envio de email
 require_once 'PHPMailer/class.phpmailer.php';
 $mail = new PHPMailer();
-$mail->setFrom($sacLoja, $nome);
-$mail->addReplyTo($email);
-$mail->addAddress($sacLoja);
-$mail->Subject = "Qual a melhor foto para meu ambiente? | $nome <$email>";
+$mail->setFrom($emailOrigem, $nomeCliente);
+$mail->addReplyTo($emailCliente);
+$mail->addAddress($emailDestino);
+$mail->Subject = "Qual a melhor foto para meu ambiente? | $nomeCliente <$emailCliente>";
 $mail->msgHTML($msgMail);
-$mail->addAttachment($foto['tmp_name'], $foto['name']);
+$mail->addAttachment($foto[ 'tmp_name' ], $foto[ 'name' ]);
 
 // Foi enviado?
-if (!$mail->send()) {
+if (! $mail->send()) {
     Saida::json('Desculpe-nos, não foi possível enviar sua imagem no momento.' . PHP_EOL . 'Tente novamente mais tarde',
         true);
 }
--- scripts.js
+++ scripts.js
@@ -3,62 +3,62 @@
  * @param form Formulário (jQuery)
  * @param pos Posição que deve ficar
  */
-var ajeitaForm = function (form, pos) {
-    form.css({
+var ajeitaForm = function ( form, pos ) {
+    form.css( {
         '-webkit-transform': 'translateX(' + pos + 'px)',
-        '-moz-transform': 'translateX(' + pos + 'px)',
-        '-ms-transform': 'translateX(' + pos + 'px)',
-        '-o-transform': 'translateX(' + pos + 'px)',
-        'transform': 'translateX(' + pos + 'px)'
-    });
+        '-moz-transform'   : 'translateX(' + pos + 'px)',
+        '-ms-transform'    : 'translateX(' + pos + 'px)',
+        '-o-transform'     : 'translateX(' + pos + 'px)',
+        'transform'        : 'translateX(' + pos + 'px)'
+    } );
 
 };
 
 /**
  * Iniciando documento
  */
-$(function () {
+$( function () {
 
     // Formulário e botão voltar
-    var form = $('#form-lamp');
-    var btnVoltar = form.find('.voltar').eq(0);
+    var form           = $( '#form-lamp' );
+    var btnVoltar      = form.find( '.voltar' ).eq( 0 );
     var btnVoltarTexto = btnVoltar.html();
 
     // Houve escolha de foto?
-    $('#foto').change(function () {
+    $( '#foto' ).change( function () {
         // Próxima tela
-        ajeitaForm(form, -305);
-    });
+        ajeitaForm( form, -305 );
+    } );
 
     // Formulário foi submetido?
-    form.submit(function (e) {
+    form.submit( function ( e ) {
         e.preventDefault();
 
         // Email é válido?
-        if (!$('#email').val().match(/^[a-z0-9\._-]+@[a-z0-9_-]+\.[a-z0-9\._-]+$/i)) {
-            alert('Por favor, nos informe um email válido');
+        if ( !$( '#email' ).val().match( /^[a-z0-9\._-]+@[a-z0-9_-]+\.[a-z0-9\._-]+$/i ) ) {
+            alert( 'Por favor, nos informe um email válido' );
             return false;
         }
 
         // Pegando elementos de espera e url do form
-        var wait = $('#wait');
-        var url = form.attr('action');
+        var wait = $( '#wait' );
+        var url  = form.attr( 'action' );
 
         // Pegando todos os dados, incluindo arquivo, do form
-        var formData = new FormData(this);
+        var formData = new FormData( this );
 
         // Aguarde...
         wait.fadeIn();
 
         // Enviando dados
-        $.ajax({
-            url: url,
-            type: 'POST',
+        $.ajax( {
+            url     : url,
+            type    : 'POST',
             dataType: "JSON",
 
             // Dados essenciais para upload de arquivos
-            data: formData,
-            cache: false,
+            data       : formData,
+            cache      : false,
             contentType: false,
             processData: false,
 
@@ -66,58 +66,58 @@ $(function () {
             xhr: function () {
                 var myXhr = $.ajaxSettings.xhr();
                 // Avalia se tem suporte a propriedade upload
-                if (myXhr.upload) {
-                    myXhr.upload.addEventListener('progress', function (progress) {
+                if ( myXhr.upload ) {
+                    myXhr.upload.addEventListener( 'progress', function ( progress ) {
                         // Pega o percentual de envio
                         // progress.loaded = quanto já foi enviado (bytes)
                         // progress.total = tamanho do arquivo (bytes)
-                        var percentual = Math.round(progress.loaded / progress.total * 100) + '%';
-                        wait.html('Enviando... (' + percentual + ')');
-                    }, false);
+                        var percentual = Math.round( progress.loaded / progress.total * 100 ) + '%';
+                        wait.html( 'Enviando... (' + percentual + ')' );
+                    }, false );
                 }
                 return myXhr;
             },
 
             // Sucesso
-            success: function (dados) {
+            success: function ( dados ) {
                 // Colocando mensagem de retorno
-                var retorno = $('#retorno');
-                retorno.html(dados.msg.replace(/[\n\r]+/ig, '<br>'));
+                var retorno = $( '#retorno' );
+                retorno.html( dados.msg.replace( /[\n\r]+/ig, '<br>' ) );
 
                 // Mensagem é de erro?
-                if (dados.erro) {
+                if ( dados.erro ) {
                     // Coloca classe de erro na div de retorno
-                    retorno.addClass('erro');
+                    retorno.addClass( 'erro' );
                     // Altera o botão de voltar para que leve o cliente pra tela inicial
                     btnVoltar
-                        .html('Enviar outra Foto')
-                        .unbind('click')
-                        .bind('click', function (e) {
+                        .html( 'Enviar outra Foto' )
+                        .unbind( 'click' )
+                        .bind( 'click', function ( e ) {
                             e.preventDefault();
-                            ajeitaForm(form, 0);
-                        });
+                            ajeitaForm( form, 0 );
+                        } );
                 } else
                 // Mantém o obtão voltar como original
                     btnVoltar
-                        .html(btnVoltarTexto)
-                        .unbind('click');
+                        .html( btnVoltarTexto )
+                        .unbind( 'click' );
 
                 // Some com o aguarde
                 wait.fadeOut();
 
                 // Mostra mensagem de retorno
-                ajeitaForm(form, -610);
+                ajeitaForm( form, -610 );
 
-                console.log(dados.msg);
-                console.log(dados.erro);
+                console.log( dados.msg );
+                console.log( dados.erro );
             },
 
             // Falha
-            error: function (retorno) {
-                console.error(retorno);
+            error: function ( retorno ) {
+                console.error( retorno );
             }
-        });
+        } );
 
-    });
+    } );
 
-});
+} );
